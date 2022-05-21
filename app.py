cc85645791f4a7efa33955724e41f1be9cb2bd30
Mateusz Jarliński <m.jarlinski@o2.pl>
2022-05-21 19:11:16 +0200

    Add app.py

--- app.py
+++ app.py
@@ -4,5 +4,16 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/mypage')
-def mypage():
+@app.route("/mypage/me", methods=['GET'])
+def me():
+    return render_template("strona1.html")
+
+@app.route("/mypage/contact", methods=['GET', "POST"])
+def kontakt():
+    if request.method == 'GET':
+       print("We received GET")
+       return render_template("strona2.html")
+    elif request.method == 'POST':
+       print("We received POST")
+       print(request.form)
+       return(request.form)
